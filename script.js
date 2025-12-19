8ff1f3a896c624ac5ae18a2ec5bf0fd45a2d2c52
rajat5596 <parraj.sri@gmail.com>
2025-12-19 12:35:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -689,15 +689,4 @@ window.onload = () => {
     };
 }; // window.onload का क्लोजिंग ब्रैकेट
 
-// 5.4 Service Worker Registration (PWA)
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('sw.js')
-            .then(registration => {
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-            })
-            .catch(err => {
-                console.log('ServiceWorker registration failed: ', err);
-            });
-    });
-}
+
